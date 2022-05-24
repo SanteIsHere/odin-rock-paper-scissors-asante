aa03d70aee7823bd902daf0fad638e71705df542
SanteIsHere <r.a.frye00@gmail.com>
2022-05-24 14:22:34 -0400

    Finished with the RPS project script

--- script.js
+++ script.js
@@ -14,17 +14,54 @@ function playerSelection() {
 
 // Function to play a round of RPS
 function playRound(playerSel=playerSelection(), compSel=computerPlay()) {
-    let compWin = "Computer wins!";
-    let playerWin = "Player wins!";
+    let compWin = "Computer wins the round!";
+    let playerWin = "Player wins the round!";
     let tie = "It's a tie!";
 
-    console.log(`Player chooses: ${playerSel}\nComputer chooses: ${compSel}`)
+    console.log(`Player chooses: ${playerSel}\nComputer chooses: ${compSel}`);
 
     if (compSel === playerSel) {
-        return tie;
-    } else if ((compSel == "Rock" && playerSel == "Scissors") || (compSel == "Scissors" && playerSel == "Paper")) {
-        return compWin;
+        console.log(tie);
+        return 0;
+    } else if ((compSel == "Rock" && playerSel == "Scissors") 
+    || (compSel == "Scissors" && playerSel == "Paper")) {
+        console.log(`${compWin}\n`);
+        return 1;
     } else {
-        return playerWin;
+        console.log(`${playerWin}\n`);
+        return 2;
     }
 }
+
+// Function to play the game
+function playGame() {
+    let computerScore = 0;
+    let playerScore = 0;
+    // Play 5 rounds
+    for (let i = 0; i < 5; i++) {
+        let result = playRound();
+        switch (result) {
+            case 0:
+                console.log(`Tie...No points for computer or player...\nComputer Score: ${computerScore}\nPlayer Score: ${playerScore}`);
+                console.log("---- END ROUND ----");
+            case 1:
+                // Computer wins round
+                computerScore += 1;
+                console.log(`Computer wins the round!\nComputer Score: ${computerScore}\nPlayer Score: ${playerScore}`);
+                console.log("---- END ROUND ----");
+            case 2:
+                // Player wins round
+                playerScore += 1;
+                console.log(`Player wins the round!\nComputer Score: ${computerScore}\nPlayer Score: ${playerScore}`);
+                console.log("---- END ROUND ----");
+        }
+    }
+    if (computerScore === playerScore) {
+        console.log("Tie game!")
+    } else if (computerScore > playerScore) {
+        console.log("Computer wins!")
+    } else {
+        console.log("Player wins!")
+    }
+}
+playGame()
