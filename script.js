e08b6088fcb29a1501e8838ecb5d09d1b26779de
SanteIsHere <r.a.frye00@gmail.com>
2022-05-24 12:12:46 -0400

    Finish playRound function, capitalize helper function, playerSelection function and playRound function

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// The computer chooses a random selection from the array
 function computerPlay() {
-    return ['Rock', 'Paper', 'Scissors'][Math.round(Math.random() * 2)]
+    return ["Rock", "Paper", "Scissors"][Math.round(Math.random() * 2)]
+}
+
+// Helper function to format playerSelection
+function capitalize(str) {
+    return `${str[0].toUpperCase()}${str.slice(1, str.length).toLowerCase()}`
+}
+
+function playerSelection() {
+    return capitalize(prompt("Enter your choice (Rock/Paper/Scissors): "))
+}
+
+// Function to play a round of RPS
+function playRound(playerSel=playerSelection(), compSel=computerPlay()) {
+    let compWin = "Computer wins!";
+    let playerWin = "Player wins!";
+    let tie = "It's a tie!";
+
+    console.log(`Player chooses: ${playerSel}\nComputer chooses: ${compSel}`)
+
+    if (compSel === playerSel) {
+        return tie;
+    } else if ((compSel == "Rock" && playerSel == "Scissors") || (compSel == "Scissors" && playerSel == "Paper")) {
+        return compWin;
+    } else {
+        return playerWin;
+    }
 }
