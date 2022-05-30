2f3c4376311e49a0b33ea8f5721c23e7eb166a80
SanteIsHere <r.a.frye00@gmail.com>
2022-05-29 20:24:38 -0400

    General changes

--- script.js
+++ script.js
@@ -66,33 +66,57 @@ function playGame() {
 }
 
 // ===== UI Section ===== //
+function playRoundUI(playerSel=playerSelection(), compSel=computerPlay()) {
+    let compWin = "Computer wins the round!";
+    let playerWin = "Player wins the round!";
+    let tie = "It's a tie!";
 
-function game(rounds) {
+    console.log(`Player chooses: ${playerSel}\nComputer chooses: ${compSel}`);
+
+    if (compSel === playerSel) {
+        output.textContent = tie
+        return 0;
+    } else if ((compSel == "Rock" && playerSel == "Scissors") 
+    || (compSel == "Scissors" && playerSel == "Paper")) {
+        output.textContent = `${compWin}`;
+        return 1;
+    } else {
+        output.textContent = `${playerWin}`;
+        return 2;
+    }
+}
+
+function game(rounds, output) {
     for (let i = 0; i < rounds; i++) {
-        let result = playRound();
+        let result = playRoundUI();
         switch (result) {
             case 0:
-                console.log(`Tie...No points for computer or player...\nComputer Score: ${computerScore}\nPlayer Score: ${playerScore}`);
-                console.log("---- END ROUND ----");
+                output.textContent = `Tie...No points for computer or player...\nComputer Score: ${computerScore}\nPlayer Score: ${playerScore}`;
+                output.textContent = "---- END ROUND ----";
             case 1:
                 // Computer wins round
                 computerScore += 1;
-                console.log(`Computer wins the round!\nComputer Score: ${computerScore}\nPlayer Score: ${playerScore}`);
-                console.log("---- END ROUND ----");
+                output.textContent = `Computer wins the round!\nComputer Score: ${computerScore}\nPlayer Score: ${playerScore}`;
+                output.textContent = "---- END ROUND ----";
             case 2:
                 // Player wins round
                 playerScore += 1;
-                console.log(`Player wins the round!\nComputer Score: ${computerScore}\nPlayer Score: ${playerScore}`);
-                console.log("---- END ROUND ----");
+                output.textContent = `Player wins the round!\nComputer Score: ${computerScore}\nPlayer Score: ${playerScore}`;
+                output.textContent = "---- END ROUND ----";
         }
     }
     if (computerScore === playerScore) {
-        console.log("Tie game!")
+        output.textContent = "Tie game!"
     } else if (computerScore > playerScore) {
-        console.log("Computer wins!")
+        output.textContent = "Computer wins!"
     } else {
-        console.log("Player wins!")
+        output.textContent = "Player wins!"
     }
+}
+
+function getRounds() {
+    output.textContent = roundsInput.value;
+    roundsInputCont.style.display = "none"
 }
 
 function playGameUI() {
@@ -100,6 +124,8 @@ function playGameUI() {
     //  interactive UI elements
     let compScore = 0;
     let playerScore = 0;
+    let output = document.getElementById("output")
+    let roundsInput = document.getElementById("roundsInput")
     const scoreBox = document.getElementById("scoreBox")
     const compScoreUI = document.getElementById("compScoreDisplay")
     const playerScoreUI = document.getElementById("playerScoreDisplay")
@@ -113,7 +139,6 @@ function playGameUI() {
     playerChoice.style.display = "flex"
     scoreBox.style.display = "flex"
     playButton.style.display = "none"
-
-
-    game(rounds)
+    let rounds = getRounds()
+    game(rounds, output)
 }
